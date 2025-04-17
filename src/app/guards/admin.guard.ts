37d37e43610fe45778565a25d762882ac4266600
unknown <burellert@gmail.com>
2025-04-17 23:01:00 +0200

    fix authguard + logo

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -3,17 +3,15 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  // Injection du service Router pour rediriger si l'utilisateur n'est pas admin
+
   const router = inject(Router);
-
-  // Récupération du rôle de l'utilisateur depuis localStorage
   const userRole = localStorage.getItem('role');
+  const token = localStorage.getItem('token');
 
-  // Si l'utilisateur n'a pas de rôle ou n'est pas admin, on le redirige vers la page de login
-  if (userRole !== 'admin') {
-    router.navigate(['/']);  // Redirection vers la page de login ou une autre page
+  if (!token || !userRole || userRole !== 'admin') {
+    router.navigate(['/']);
     return false;
+  } else {
+    return true;
   }
-
-  return true;  // Si le rôle est admin, on permet l'accès à la route protégée
 };
